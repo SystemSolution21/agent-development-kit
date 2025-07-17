65eec1a143c8097834ba8478ac815c453f8f990a
SystemSolution21 <156997764+SystemSolution21@users.noreply.github.com>
2025-07-17 23:12:48 +0900

    modified:   11_parallel_agent/system_monitor_agent/docs/system_health_report.md         new file:   12_loop_agent/README.md         new file:   12_loop_agent/linkedin_post_agent/__init__.py         new file:   12_loop_agent/linkedin_post_agent/agent.py         new file:   12_loop_agent/linkedin_post_agent/constant.py         new file:   12_loop_agent/linkedin_post_agent/subagents/__init__.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_generator/__init__.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_generator/agent.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_refiner/__init__.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_refiner/agent.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_refiner/tools.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_reviewer/__init__.py         new file:   12_loop_agent/linkedin_post_agent/subagents/post_reviewer/agent.py

---
+++ 12_loop_agent/linkedin_post_agent/constant.py
@@ -0,0 +1,9 @@
+# Gemini Model
+GEMINI_MODEL = "gemini-2.0-flash"
+
+# Maximum refinement loop
+MAX_ITERATIONS = 5
+
+# Post Length Requirements
+MIN_POST_LENGTH = 1000
+MAX_POST_LENGTH = 1500
---
+++ 12_loop_agent/linkedin_post_agent/subagents/post_generator/__init__.py
@@ -0,0 +1,3 @@
+from .agent import initial_post_generator
+
+__all__: list[str] = ["initial_post_generator"]
